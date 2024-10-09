9278abc826b39dc8d0534fe6ecb437d1b25ab81e
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-10-09 09:21:47 -0300

    changes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿// Screen sound
 string message = "Boas vindas ao screen sound!";
-Console.WriteLine(message);
+
+void ShowMessage() 
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(message);
+}
+
+void ShowMenuOptions() 
+{
+    Console.Write("Digite 1 para registrar uma banda");
+    Console.Write("Digite 2 para mostrar todas as bandas");
+    Console.Write("Digite 3 para avaliar uma banda");
+    Console.Write("Digite 4 para exibir a média de uma banda");
+    Console.Write("Digite 0 para sair");
+}
+
+ShowMessage();
+ShowMenuOptions();
+
