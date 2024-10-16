88c50ec9e78afbf704370be0416924a29b6adbe7
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-10-16 11:32:05 -0300

    if else

--- Program.cs
+++ Program.cs
@@ -15,11 +15,21 @@ void ShowMessage()
 
 void ShowMenuOptions() 
 {
-    Console.Write("Digite 1 para registrar uma banda");
-    Console.Write("Digite 2 para mostrar todas as bandas");
-    Console.Write("Digite 3 para avaliar uma banda");
-    Console.Write("Digite 4 para exibir a média de uma banda");
-    Console.Write("Digite 0 para sair");
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite uma opção: ");
+    int option = int.Parse(Console.ReadLine());
+    if (option == 1) 
+    {
+        Console.WriteLine("Você digitou a opção " + option);
+    } else if (option == 2) 
+    {
+        Console.WriteLine("Você digitou a opção " + option);
+    }
 }
 
 ShowMessage();
