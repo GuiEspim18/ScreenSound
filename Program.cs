3e63db4acd0880f8837ccb43c67e7e5644a8680d
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-10-16 12:34:40 -0300

    for loop

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿// Screen sound
-string message = "Boas vindas ao screen sound!";
 
-void ShowMessage() 
+List<string> bands = new();
+
+void ShowLogo() 
 {
     Console.WriteLine(@"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -10,11 +11,12 @@ void ShowMessage()
 ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
 ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
-    Console.WriteLine(message);
 }
 
 void ShowMenuOptions() 
 {
+    Console.Clear();
+    ShowLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -22,12 +24,12 @@ void ShowMenuOptions()
     Console.WriteLine("Digite 0 para sair");
 
     Console.Write("\nDigite uma opção: ");
-    int option = int.Parse(Console.ReadLine());
+    int option = int.Parse(Console.ReadLine()!);
     
     switch (option) 
     {
-        case 1: Console.WriteLine("Você escolheu a opção " + option); break;
-        case 2: Console.WriteLine("Você escolheu a opção " + option); break;
+        case 1: RegisterBand(); break;
+        case 2: ShowBands(); break;
         case 3: Console.WriteLine("Você escolheu a opção " + option); break;
         case 4: Console.WriteLine("Você escolheu a opção " + option); break;
         case 0: Console.WriteLine("Você escolheu a opção " + option); break;
@@ -35,6 +37,30 @@ void ShowMenuOptions()
     }
 }
 
-ShowMessage();
+void RegisterBand() 
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda que você deseja registrar: ");
+    string band = Console.ReadLine()!;
+    bands.Add(band);
+    Console.WriteLine($"A banda {band} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    ShowMenuOptions();
+}
+
+void ShowBands() 
+{
+    Console.Clear();
+    Console.WriteLine("Bandas Cadastradas\n");
+    for (int i = 0; i < bands.Count; i++) 
+    {
+        Console.WriteLine($"{i}. {bands[i]}");
+    }
+    Console.Write("\nPressione qualquer tecla para volar ao menu principal");
+    Console.ReadKey();
+    ShowMenuOptions();
+}
+
 ShowMenuOptions();
 
